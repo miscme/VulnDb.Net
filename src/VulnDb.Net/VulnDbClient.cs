7a210e3bf9dbb968443414ea4b62aa2e05da211d
miscme <miscme1@pm.me>
2020-09-01 16:30:12 +0200

    Setup base class

---
+++ src/VulnDb.Net/VulnDbClient.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace VulnDb.Net
+{
+    public class VulnDbClient : IDisposable
+    {
+        private const string BaseUrl = "";
+        private string ClientId { get; }
+        private string ClientSecret { get; }
+
+        public VulnDbClient(string clientId, string clientSecret)
+            : this(url: BaseUrl, clientId, clientSecret)
+        {
+        }
+
+        public VulnDbClient(string url, string clientId, string clientSecret)
+        {
+            ClientId = clientId;
+            ClientSecret = clientSecret;
+            var clientHandler = new HttpClientHandler();
+            var cookieContainer = new CookieContainer();
+            clientHandler.CookieContainer = cookieContainer;
+            var client = new HttpClient(clientHandler)
+            {
+                BaseAddress = new Uri(BaseUrl)
+            };
+            client.DefaultRequestHeaders.Referrer = new Uri(url);
+        }
+
+        public async Task GetTokenAsync(string clientId, string clientSecret)
+        {
+            
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
