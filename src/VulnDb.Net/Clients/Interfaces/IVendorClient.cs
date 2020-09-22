f81675ee9d1d967e57d00871e9666fae317cbacd
miscme <miscme1@pm.me>
2020-09-22 11:56:07 +0200

    Moving Vendor Client, introducting empty connector

--- src/VulnDb.Net/Clients/Interfaces/IVendorClient.cs
+++ src/VulnDb.Net/Clients/Interfaces/IVendorClient.cs
@@ -1,7 +1,16 @@
-﻿namespace VulnDb.Net.Clients
+﻿using System.Threading.Tasks;
+using VulnDb.Net.Models;
+using VulnDb.Net.Models.Vendor;
+
+namespace VulnDb.Net.Clients
 {
     public interface IVendorClient
     {
-        
+        Task<VulnDbResponse<VendorInformations>> GetVendorByIdAsync(int vendorId, VendorInformationOptions options = null);
+        Task<VulnDbResponse<VendorInformations>> GetVendorByNameAsync(string vendorName, VendorInformationOptions options = null);
+        Task<VulnDbResponse<VendorInformations>> GetVendorByProductIdAsync(int productId, VendorInformationOptions options = null, int size = 20, int page = 1);
+        Task<VulnDbResponse<VendorInformations>> GetVendorModifiedAsync(string startDate = "", string endDate = "", VendorInformationOptions options = null, int size = 1, int page = 1);
+        Task<VulnDbResponse<VendorInformations>> GetVendorNewAsync(string startDate = "", string endDate = "", VendorInformationOptions options = null, int size = 1, int page = 1);
+        Task<VulnDbResponse<VendorInformations>> GetVendorsAsync(VendorInformationOptions options = null, int size = 20, int page = 1);
     }
 }
---
+++ src/VulnDb.Net/IAPIConnector.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VulnDb.Net
+{
+    interface IAPIConnector
+    {
+    }
+}
